7bbda5080d9896cd0cd5025f656f52a59926ec8b
Christian John Roa <roacj.cr@gmail.com>
2026-02-27 11:48:22 +0800

    Call login api

--- HrLeaveManagementUI/src/services/authService.jsx
+++ HrLeaveManagementUI/src/services/authService.jsx
@@ -1,18 +1,16 @@
 import React, { useState } from 'react';
 import axios from 'axios';
 
-const login = async () => {
-      try {
-        const response = await axios.post('https://api.example.com/posts');
-        setData(response.data);
-      } catch (error) {
-        console.error("Error fetching data:", error);
-      } finally {
-        setLoading(false);
-      }
-    };
+export const login = async (email, password) => {
+  try {
 
+    const response = await axios.post('https://localhost:7047/api/Auth/login', {
+      email: email,
+      password: password
+    });
 
-export default [
-    login
-];
+    console.log(response.data);
+  } catch (error) {
+    console.error("Error fetching data:", error);
+  }
+};
