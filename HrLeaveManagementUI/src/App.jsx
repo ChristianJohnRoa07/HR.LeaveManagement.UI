1d2856f825b681b2f2adf8057a7504114d585edd
Christian John Roa <roacj.cr@gmail.com>
2026-02-27 11:47:29 +0800

    Implement routing

--- HrLeaveManagementUI/src/App.jsx
+++ HrLeaveManagementUI/src/App.jsx
@@ -1,18 +1,28 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
 
-import { Print, login} from './services/authService'
+import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
+
+// Components
+import LoginPage from './interface/login/components/LoginPage'
+import RegisterPage from './interface/register/components/RegisterPage'
+
 
 function App() {
 
   return (
-    <>
-      <h1>Vite + React</h1>
+    <Router>
+      <Routes>
+        /* Redirect empty path to login */
+        <Route path="/" element={<Navigate to="/login" />} />
 
-      <Print username="JSnow" password="WinterIsComing123" />
-    </>
+        /* Define your pages */
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/register" element={<RegisterPage />} />
+
+        <Route path="*" element={<h1>404 - Page Not Found</h1>} />
+
+      </Routes>
+
+    </Router>
   )
 }
 
