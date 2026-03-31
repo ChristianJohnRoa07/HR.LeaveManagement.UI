a23965e2099398c939dd9a6ab97d106657becaaf
Christian John Roa <roacj.cr@gmail.com>
2026-03-31 15:36:38 +0800

    Implement user role filtering for admin and employee routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,29 @@
 
 import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
+import { useSelector } from 'react-redux';
 
 // Components
 import LoginPage from './interface/login/components/LoginPage'
 import RegisterPage from './interface/register/components/RegisterPage'
-import Dashboard from './interface/homepage/dashboard/components/Dashboard';
-import ApplyLeave from './interface/homepage/dashboard/components/ApplyLeave';
+import Dashboard from './interface/employeeHomepage/dashboard/components/Dashboard';
+import ApplyLeave from './interface/employeeHomepage/dashboard/components/ApplyLeave';
 
 
+const DashboardGate = ({ user }) => {
+  if (!user) return <Navigate to="/login" />;
+
+  // Logic: Redirect based on role
+  return user.role === 'Administrator' ? <h1>ADMIN</h1> : <Dashboard />;
+};
+
+const ProtectedRoute = ({ user, children }) => {
+  if (!user) return <Navigate to="/login" replace />;
+  return children;
+};
 
 function App() {
+
+  const user = useSelector((state) => state.auth.user);
 
   return (
     <Router>
@@ -17,11 +31,21 @@ function App() {
         /* Redirect empty path to login */
         <Route path="/" element={<Navigate to="/login" />} />
 
-        /* Define your pages */
+        /* Public routes */
         <Route path="/login" element={<LoginPage />} />
         <Route path="/register" element={<RegisterPage />} />
-        <Route path='/dashboard' element={<Dashboard />}/>
-        <Route path='/apply-leave' element={<ApplyLeave />}/>
+
+
+        <Route path="/dashboard" element={<DashboardGate user={user} />} />
+
+        <Route
+          path='/apply-leave'
+          element={
+            <ProtectedRoute user={user}>
+              <ApplyLeave />
+            </ProtectedRoute>
+          }
+        />
 
         <Route path="*" element={<h1>404 - Page Not Found</h1>} />
 
